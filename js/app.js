5d39f0f92168a076eb9481e1ef8a45ea9d744ebf
tahseensikder <tahseensikder@gmail.com>
2022-07-15 09:17:31 +0600

    Aos,preloader,BackToTop

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
+
+$(window).on('load',function(){
+    $('.preloader').fadeOut(1000)
+})
 $(function(){
+    //fixed Nav
     $(window).on('scroll',function(){
         if ($(window).scrollTop()>$('nav').height()){
             $('nav').addClass('menu_animate')
@@ -7,4 +12,35 @@ $(function(){
             $('nav').removeClass('menu_animate')
         }
     })
+
+    //backToTop
+    // $(window).on('scroll',function(){
+    //     if($(this).scrollTop() > 300){
+    //      $('.backToTop').fadeIn(1000);
+    //     }
+    //     else {
+    //      $('.backToTop').fadeOut(1000);
+    //     }
+    // })
+    // $('.backToTop').on('click',function(){
+    //     $('html,body').animate({
+    //         scrollTop:0,
+    //     },1000)
+    // })
+    $(window).on('scroll',function(){
+        if($(this).scrollTop() > 300){
+            $('.backToTop').fadeIn(1000)
+        }
+        else{
+            $('.backToTop').fadeOut(1000)
+        }
+    })
+    $('.backToTop').on('click',function(){
+        $('html,body').animate({
+            scrollTop:0
+        },1000)
+    })
+
 })
+AOS.init();
+
